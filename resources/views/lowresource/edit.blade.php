8a2d8f627837c06c353f61a97183b316b41f8ea5
amsalu-tadesse <tadesseamsalu@gmail.com>
2024-06-23 10:41:49 +0300

    two calendars added

--- resources/views/lowresource/edit.blade.php
+++ resources/views/lowresource/edit.blade.php
@@ -476,22 +476,6 @@
                                                             <tr data-id="{{ $fo->id + 1}}">
                                                                 <td>
 
-                                                                    <?php
-
-                                                                    $fdate = Carbon::today();
-
-                                                                    $tdate = Carbon::today();
-                                                                   // $experience = "Extra";//$fo->positionyouworked;
-                                                                    $days = $tdate->diffInDays($fdate);
-                                                                    $months = $tdate->diffInMonths($fdate);
-
-                                                                    $years = $tdate->diffInYears($fdate);
-
-                                                                    $time = $tdate->diff($fdate);
-
-                                                                  //  echo $time->y, '-', $time->m, '-', $time->d, ' (', $experience, ')';
-                                                                    ?>
-
                                                                    From <input type="text" name="from" id="from" placeholder="yyyy-mm-dd"> <br>
                                                                    To &nbsp; &nbsp;&nbsp;&nbsp;<input type="text" name="to" id="to" placeholder="yyyy-mm-dd">
 
@@ -502,7 +486,7 @@
                                                                     <div class="col-md-10">
 
                                                                         <select
-                                                                            class="form-control custom-select   mt-15" id="selection2">
+                                                                            class="form-control custom-select newselects select mt-15" id="selection2">
                                                                             <option selected>Select</option>
                                                                             <option value="0">0</option>
                                                                             <option value="0.5">1/2</option>
@@ -511,10 +495,37 @@
 
                                                                     </div>
                                                                 </td>
-                                                                <td id="add2"></td>
+                                                                <td id="add"></td>
                                                             </tr>
 
-                                                            
+
+                                                            <tr data-id="{{ $fo->id + 2}}">
+                                                                <td>
+
+                                                                   From <input type="text" name="from2" id="from2" placeholder="yyyy-mm-dd"> <br>
+                                                                   To &nbsp; &nbsp;&nbsp;&nbsp;<input type="text" name="to2" id="to2" placeholder="yyyy-mm-dd">
+
+
+                                                                </td>
+
+                                                                <td>
+                                                                    <div class="col-md-10">
+
+                                                                        <select
+                                                                            class="form-control select newselects custom-select   mt-15" id="selection3">
+                                                                            <option selected>Select</option>
+                                                                            <option value="0">0</option>
+                                                                            <option value="0.5">1/2</option>
+                                                                            <option value="1">1</option>
+                                                                        </select>
+
+                                                                    </div>
+                                                                </td>
+                                                                <td id="add"></td>
+                                                            </tr>
+
+
+
                                                             <tr>
                                                                 <td colspan="2" class="text-center">ድምር</td>
                                                                 {{-- <td></td> --}}
@@ -560,7 +571,6 @@
 @endsection
 
 
-
 @section('javascript')
     <script>
         $(document).ready(function() {
@@ -568,12 +578,16 @@
             var totalMonth = 0;
             var totalDay = 0;
 
+      
+
         // Get the current date
         var currentDate = new Date().toISOString().substr(0, 10);
 
         // Set the value of the input field
         document.getElementById("from").value = currentDate;
         document.getElementById("to").value = currentDate;
+        document.getElementById("from2").value = currentDate;
+        document.getElementById("to2").value = currentDate;
 
 
 
@@ -587,15 +601,31 @@
                 $('.select').each(function() {
                     var selectedValue = parseFloat($(this).val());
 
-                    var data = $(this).closest('tr').data('id');
-                    var myData = {!! json_encode($forms) !!};
+                    var data = null;
+                    var myData = null;
+                    var row = null;
+                    var startdate = null;
+                    var enddate = null;
 
-                    var row = myData.find(function(obj) {
+                    if($(this).hasClass('newselects'))
+                    {
+                        console.log("yes")
+                         startdate = new Date($("#from2").val());
+                         enddate = new Date($("#to2").val());
+                    }
+                    else 
+                    {
+                        console.log("NO")
+                     data = $(this).closest('tr').data('id');
+                     myData = {!! json_encode($forms) !!};
+                     row = myData.find(function(obj) {
                         return obj.id === data;
                     });
+                     startdate = new Date(row.startingDate);
+                     enddate = new Date(row.endingDate);
+                    }
 
-                    var startdate = new Date(row.startingDate);
-                    var enddate = new Date(row.endingDate);
+                    
                     // var date = new Date(row.startingDate);
                     // console.log(startdate);
                     // var unix = Math.floor(date.getTime() / 1000);
@@ -632,7 +662,7 @@
                     if (selectedValue == 0.5) {
                         if (yearDifferenceb % 2 != 0) {
                             yearDifference = parseInt(yearDifference);
-                            console.log(yearDifference);
+                            // console.log(yearDifference);
                             // monthDifference+=6
                             monthDifference = 6 + (monthDifference);
                             //    console.log(monthDifference);
@@ -662,14 +692,6 @@
 
 
                     }
-                    console.log(yearDifference);
-                    console.log(monthDifference);
-                    console.log(dayDifference);
-
-
-
-
-
 
                     if (dayDifference < 0) {
                         dayDifference += 30;
@@ -680,9 +702,7 @@
                         monthDifference += 12;
                         yearDifference -= 1;
                     }
-                    // console.log(dayDifference);
-                    // console.log(monthDifference);
-                    // console.log(yearDifference);
+                    
                     var all = yearDifference + '-' + monthDifference + '-' + dayDifference;
 
 
@@ -699,109 +719,17 @@
                         totalMonth = totalMonth - 12;
                     }
 
-
                     $(this).closest('tr').find('#add').text(all);
                 });
 
-                var total = totalYear + '-' + totalMonth + '-' + totalDay;
+                totalYear = totalYear;
+                totalMonth = totalMonth;
+                totalDay = totalDay;
+
+               var total = totalYear + '-' + totalMonth + '-' + totalDay;
                 $('#total-year').text(total);
-            });
-
-
-
-
-            $('#selection2').on('change', function() {
-                // Reset the totals when a new value is selected
-                totalYear = 0;
-                totalMonth = 0;
-                totalDay = 0;
-
-                // Iterate over each select element and calculate the sum
                 
-                    var selectedValue = parseFloat($(this).val());
-                    var startdate = new Date($("#from").val());
-                    var enddate = new Date($("#to").val());
-
-                    const years = startdate.getFullYear();
-                    const months = startdate.getMonth() + 1;
-                    const days = startdate.getDate();
-                    const years2 = enddate.getFullYear();
-                    const months2 = enddate.getMonth() + 1;
-                    const days2 = enddate.getDate();
-                    let dayDifferenceb = (days2 - days);
-                    let monthDifferenceb = (months2 - months);
-                    let yearDifferenceb = (years2 - years);
-                    if (dayDifferenceb < 0) {
-                        dayDifferenceb += 30;
-                        monthDifferenceb -= 1;
-
-                    } else {
-                        dayDifferenceb = (days2 - days);
-                        monthDifferenceb = monthDifferenceb;
-
-                    }
-                    if (monthDifferenceb < 0) {
-                        monthDifferenceb += 12;
-                        yearDifferenceb -= 1;
-                    } else {
-                        monthDifferenceb = (months2 - months);
-                        yearDifferenceb = yearDifferenceb;
-                    }
-
-                    let dayDifference = dayDifferenceb * selectedValue;
-                    let monthDifference = monthDifferenceb * selectedValue;
-                    let yearDifference = yearDifferenceb * selectedValue;
-
-                    if (selectedValue == 0.5) {
-                        if (yearDifferenceb % 2 != 0) {
-                            yearDifference = parseInt(yearDifference);
-                            console.log(yearDifference);
-                            // monthDifference+=6
-                            monthDifference = 6 + (monthDifference);
-                            //    console.log(monthDifference);
-                            if (monthDifference >= 12) {
-                                monthDifference = 0;
-                                yearDifference = yearDifference + 1
-
-                            }
-
-
-                        } else {
-                            yearDifference = parseInt(yearDifference);
-                            monthDifference = monthDifference;
-
-                        }
-                        if (monthDifferenceb % 2 != 0) {
-                            monthDifference = parseInt(monthDifference)
-                            dayDifference = dayDifference + 15
-
-                        } else {
-                            monthDifference = parseInt(monthDifference)
-                            dayDifference = dayDifference
-                        }
-                        if (dayDifferenceb % 2 != 0) {
-                            dayDifference = parseInt(dayDifference);
-                        }
-
-
-                    }
-
-                    if (dayDifference < 0) {
-                        dayDifference += 30;
-                        monthDifference -= 1;
-                    }
-
-                    if (monthDifference < 0) {
-                        monthDifference += 12;
-                        yearDifference -= 1;
-                    }
-                  
-                    var all = yearDifference + '-' + monthDifference + '-' + dayDifference;
-                    $(this).closest('tr').find('#add2').text(all);
-               
             });
-
-
 
 
         });
@@ -810,3 +738,4 @@
 
     </script>
 @endsection
+
